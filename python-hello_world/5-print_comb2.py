d834a1c010d23dd95b7de525e3eb9ff36f29e706
Roza Alene <rozaalene@gmail.com>
2023-07-19 06:11:56 -0700

    remove space

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,4 +3,4 @@ for x in range(100):
     if x < 10:
         print("0{}, ".format(x), end ='')
     else:
-        print("99 \n" if x == 99 else "{}, ".format(x), end = '')
+        print("99\n" if x == 99 else "{}, ".format(x), end = '')
