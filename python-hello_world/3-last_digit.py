24c4be914b63e99ed8543b141d68273e497abba9
Roza Alene <rozaalene@gmail.com>
2023-07-19 03:45:08 -0700

    remove else condition

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,10 +10,8 @@ if number > 0:
         print("Last digit of {} is {} and is 0".format(number, last_digit))
     else:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
-elif number < 0:
+else:
     if last_digit == 0:
         print("Last digit of {} is {} and is 0".format(number, last_digit))
     else:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, -last_digit))
-else:
-    print ("TypeError")
