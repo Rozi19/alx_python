6d489bf625b947e542c2155707b59b4db2aee5dc
Roza Alene <rozaalene@gmail.com>
2023-07-19 06:09:46 -0700

    useing to print only

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,7 +3,4 @@ for x in range(100):
     if x < 10:
         print("0{}, ".format(x), end ='')
     else:
-        if x > 9 and x < 99:
-            print("{}, ".format(x), end = '')
-        else:
-            print("{}".format(x))
+        print("99 \n" if x == 99 else "{}, ".format(x), end = '')
