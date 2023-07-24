c1e0c112cb7fd9823eecf44b969af6ce81b74947
Roza Alene <rozaalene@gmail.com>
2023-07-24 06:04:15 -0700

    remove script name from the list

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
-    x = len(sys.argv)
+    x = len(sys.argv[1:])
     if x == 0:
         print("0 arguments.")
     elif x == 1:
         print("{} argument:".format(x))
-        for i in range(1, x):
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
     else:
         print("{} arguments:".format(x))
-        for i in range(1, x):
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
 if __name__ == "__main__":
     main()
