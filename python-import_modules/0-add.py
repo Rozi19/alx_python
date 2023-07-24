8b6dc6c5ff0b80bc039f20bc9baddb66f73d694b
Roza Alene <rozaalene@gmail.com>
2023-07-24 00:32:30 -0700

    Adding main function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-sum = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    main()
