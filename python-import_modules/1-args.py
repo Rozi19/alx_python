c3b5fbc41022b6117d0265cde94c7c567974942f
Roza Alene <rozaalene@gmail.com>
2023-07-24 05:33:59 -0700

    Adding main

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-x = len(sys.argv)
-if x == '':
-    print("0 arguments.")
-elif (x) == 1:
-    print("{} argument:".format(x))
-    for i in range(1, x):
-        print("{}: {}".formt(i, sys.argv[i]))
-else:
-    print("{} arguments:".format(x))
-    for i in range(1, x):
-        print("{}: {}".format(i, sys.argv[i]))
+def main():
+    x = len(sys.argv)
+    if x == 0:
+        print("0 arguments.")
+    elif x == 1:
+        print("{} argument:".format(x))
+        for i in range(1, x):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments:".format(x))
+        for i in range(1, x):
+            print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    main()
