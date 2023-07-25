fe72a80d332b2ce08d1299e6bbe0ebfbdf5144f5
Roza Alene <rozaalene@gmail.com>
2023-07-25 11:22:55 -0700

    only use print

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,2 @@
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-            for element in row:
-                print("{:d}".format(element, end=''))
-            print()
+    print(matrix)
