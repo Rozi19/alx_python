62a4607c961716f12c7b5fe4688a9637d01129f0
Roza Alene <rozaalene@gmail.com>
2023-07-25 09:21:02 -0700

    print useing format

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,3 +1,3 @@
 def print_matrix_integer(matrix=[[]]):
         for row in range(len(matrix)):
-            print(("{}").format(' '.join(str(x) for x in matrix[row]))) 
+            print(("{}").format( matrix[row]))
