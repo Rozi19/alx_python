807122af8279880ea3efcfcb9be2444888bbc192
Roza Alene <rozaalene@gmail.com>
2023-07-31 05:06:48 -0700

    try to prepare module documation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,14 @@
 class Square:
+    """
+    Represent square 
+    Attributes:
+        attr1 (str): It have size attribute
+        """
     def __init__(self, size):
-        """ why size is private in __init__
+        """
+        why size is private in __init__
         b/c it affect differnt propertyies
         Args:
             param1 (int): size of square
-        Returns: 
-            size: size of square
         """
         self.__size = size 
---
+++ python-classes/0-square_module.py
@@ -0,0 +1,7 @@
+ """ why size is private in __init__
+        b/c it affect differnt propertyies
+        Args:
+            param1 (int): size of square
+        Returns: 
+            size: size of square
+        """
