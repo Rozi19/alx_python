628bdea9d526e2cc10abbb188597395b4bc0f814
Roza Alene <rozaalene@gmail.com>
2023-07-31 03:52:44 -0700

    writing module doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,3 @@
 class Square:
-    """ simple square class """
     def __init__(self, size):
-        """ the squre size is private.The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute.
-        args:
-            param1 (int): size of the square"""
         self.__size = size 
---
+++ python-classes/my_module.py
@@ -0,0 +1,6 @@
+"""
+size is private attribute?
+
+The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately.
+args:
+    params(int): size of square
