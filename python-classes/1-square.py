954ccdd5596b998993133829d650744480892971
Roza Alene <rozaalene@gmail.com>
2023-08-01 00:49:54 -0700

    try to check if integre or not if negative or

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,30 +1,30 @@
 """ we have simple class
-    class: square
-    """
+    class: square """
+
 
 class Square:
     """ here we have a constructor call squre
     attribute: size
     """
-    def __init__(self, size=0):
-        """ puting size private """
+    def  __init__(self, size=0):
+        """ using size attribute  """
         self.size = size
-        
-        @property
-        def size(self):
-            """ it is a getter to acces a class private attribute 
+
+    @property
+    def size(self):
+        """ it is a getter to acces a class private attribute
             @property is bulit in decorection it help to modify the attribute
             return modify value """
-            return self.__size
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """ It is a setter to set the property of the value 
+    @size.setter
+    def size(self, value):
+        """ It is a setter to set the property of the value
             checking if the value is digit or not and if the value is negaative number or not"""
-            if isinstance(value, int):
-                if value >= 0:
-                    self.__size = value
-                else:
-                    raise ValueError("size must be >= 0")
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
             else:
-                raise TypeError("size must be an integer")
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
