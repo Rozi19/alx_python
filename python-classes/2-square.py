75610c807a682d09839cfea5344f1da1fa475188
Roza Alene <rozaalene@gmail.com>
2023-08-01 01:49:21 -0700

    with out getter and setter

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,28 +7,14 @@ class Square:
     attribute: size
     """
     def  __init__(self, size=0):
-        """ using size attribute  """
+        """ using size attribute  check if size is integer or not and if size is negative or not"""
         self.size = size
-        
-    @property
-    def size(self):
-        """ it is a getter to acces a class private attribute
-            @property is bulit in decorection it help to modify the attribute
-            return modify value """
-        return self.__size
-    
-    @size.setter
-    def size(self, value):
-        """ It is a setter to set the property of the value
-            checking if the value is digit or not and if the value is negaative number or not"""
-        if isinstance(value, int):
-            if value >= 0:
-                self.__size = value
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
             else:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-
     def area(self):
-        """ it return the area of the square """
         return int(self.__size) * int(self.__size)
