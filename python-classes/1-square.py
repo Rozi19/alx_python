2e0425ac77f4428f3d52e6a5f299fbf24a743445
Roza Alene <rozaalene@gmail.com>
2023-08-01 02:03:42 -0700

    change value into size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,9 +9,9 @@ class Square:
     def  __init__(self, size=0):
         """ using size attribute check if size is integer or not and if the size is negative or not """
         self.size = size
-        if isinstance(value, int):
-            if value >= 0:
-                self.__size = value
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
             else:
                 raise ValueError("size must be >= 0")
         else:
