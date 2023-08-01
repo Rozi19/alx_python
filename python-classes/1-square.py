ab962d99636934c2fae0b3b8daafb483f566b941
Roza Alene <rozaalene@gmail.com>
2023-07-31 18:08:16 -0700

    access private size not size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,13 +10,13 @@ class Square:
         """ puting size private """
         self.__size = size
         @property
-        def size(self):
+        def __size(self):
             """ it is a getter to acces a class private attribute 
             @property is bulit in decorection it help to modify the attribute
             return modify value """
             return self.__size
         @size.setter
-        def size(self, value):
+        def __size(self, value):
             """ It is a setter to set the property of the value 
             checking if the value is digit or not and if the value is negaative number or not"""
             if isinstance(value, int):
