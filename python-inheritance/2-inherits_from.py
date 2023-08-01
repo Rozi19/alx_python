499605dccbed6d7cebeb7881fc4361b8f44b9c93
Roza Alene <rozaalene@gmail.com>
2023-08-01 16:44:31 -0700

    let try to print the oposit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,9 +1,10 @@
-""" if the object is an instance of a class that inherited (directly or indirectly) from the specified class """
+""" if the object is an instance of a class that inherited 
+(directly or indirectly) from the specified class """
 
 
 def inherits_from(obj, a_class):
     """ checking the condition """
-    if issubclass(type(obj), a_class):
+    if type(obj) != a_class:
         return True
     else:
         return False
