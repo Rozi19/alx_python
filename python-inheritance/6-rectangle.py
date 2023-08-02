79d66c10083d527e8347a33efc274a6db932da23
Roza Alene <rozaalene@gmail.com>
2023-08-02 04:41:52 -0700

    can not use import so

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,7 +2,24 @@
     child class: Rectangle"""
 
 
-from 5-base_geometry import BaseGeometry
+class BaseGeometry:
+    """ creating an empty class """
+    def __init__(self):
+        """ using pass"""
+        pass
+    def area(self):
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        self.__name = name
+        self.__value = value
+        if isinstance(value, int):
+            if value > 0:
+                self.__value = value
+            else:
+                raise ValueError("{} must be greater than 0".format(self.__name))
+        else:
+            raise TypeError("{} must be an integer".format(self.__name))
 class Rectangle(BaseGeometry):
     """" the class inherit from BaseGeometry"""
     def __init__(self, width, height):
