90dadb9634bab03e693331a62891b7dc87634ab4
Roza Alene <rozaalene@gmail.com>
2023-08-01 17:06:01 -0700

    try to make them a class

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,10 +1,10 @@
-""" if the object is an instance of a class that inherited 
+""" if the object is an instance of a class that inherited
 (directly or indirectly) from the specified class """
 
 
 def inherits_from(obj, a_class):
     """ checking the condition """
-    if type(obj) != a_class:
+    if issubclass(type(obj), type(a_class)):
         return True
     else:
         return False
