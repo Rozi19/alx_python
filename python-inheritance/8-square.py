34eda97ad252f2633a41943ec43f7faf68aeb026
Roza Alene <rozaalene@gmail.com>
2023-08-02 08:48:29 -0700

    remove the rectangle class invalidate

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -8,9 +8,11 @@ class BaseGeometry:
     def __init__(self):
         """ using pass"""
         pass
+    
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         self.__name = name
         self.__value = value
@@ -21,20 +23,24 @@ class BaseGeometry:
                 raise ValueError("{} must be greater than 0".format(self.__name))
         else:
             raise TypeError("{} must be an integer".format(self.__name))
+
+
 class Rectangle(BaseGeometry):
     """" the class inherit from BaseGeometry"""
     def __init__(self, width, height):
         """" validet width and height from parent class """
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+    
     def area(self):
         """" return the area of the rectangle"""
         return self.__width * self.__height
+
     def __str__(self):
         """ returns the object representation in a string format """
         return("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
 class Square(Rectangle):
     """ the class inherite from rectangle """
     def __init__(self, size):
@@ -44,4 +50,3 @@ class Square(Rectangle):
         self.integer_validator("size", self.__size)
         self.__str__()
         self.area()
-    
