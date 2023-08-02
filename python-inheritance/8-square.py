b9119830510bd8bf6751f7ee730a8bc87c120b3d
Roza Alene <rozaalene@gmail.com>
2023-08-02 08:56:59 -0700

    remove white spaceing

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         self.__name = name
         self.__value = value
