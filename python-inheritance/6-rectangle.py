f52ac08df87ed255f5e79be602a48923579171ec
Roza Alene <rozaalene@gmail.com>
2023-08-02 04:07:53 -0700

    Rectangle

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,30 @@
+""" parent class: BaseGeometry
+    child class: Rectangle"""
+
+
+class BaseGeometry:
+    """ creating an empty class """
+    def __init__(self):
+        """ using pass"""
+        pass
+    def area(self):
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        self.__name = name
+        self.__value = value
+        if isinstance(value, int):
+            if value > 0:
+                self.__value = value
+            else:
+                raise ValueError("{} must be greater than 0".format(self.__name))
+        else:
+            raise TypeError("{} must be an integer".format(self.__name))
+class Rectangle(BaseGeometry):
+    """" the class inherit from BaseGeometry"""
+    def __init__(self, width, height):
+        """" validet width and height from parent class """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
