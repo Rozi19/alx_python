0f934d78233976ba256416371204e937bda9e6c9
Roza Alene <rozaalene@gmail.com>
2023-08-01 19:00:49 -0700

    checks the type is d/f from provided

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ checking the condition """
-    if issubclass(type(obj), a_class):
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
         return False
