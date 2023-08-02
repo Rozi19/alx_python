b57000795a37799e285bb7ee223526df8b7425ad
Roza Alene <rozaalene@gmail.com>
2023-08-02 10:11:28 -0700

    remove the init subclass

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -6,6 +6,14 @@ class BaseGeometry:
     def __init__(self):
         """ using pass"""
         pass
+    def __dir__(cls):
+        """ remove __init_subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
