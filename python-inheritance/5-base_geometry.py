7126fba2cbfd9d9fa92ad20f54c6fc13cbfd1381
Roza Alene <rozaalene@gmail.com>
2023-08-03 12:36:25 -0700

    remove __init_subclass__

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,25 +1,41 @@
 """ class: BaseGeometry """
 
 
-class BaseGeometry:
-    """ creating an empty class """
-    def __init__(self):
-        """ using pass"""
-        pass
+class BaseGeometry(type):
+    """ every thing in python is an object soo the class is also an object.
+    so if it is an object it have type.type is a metaclass, of which classes
+    are instances. Just as an ordinary object is an instance of a class, 
+    any new-style class in Python, and thus any class in Python 3, 
+    is an instance of the type metaclass """
     def __dir__(cls):
-        """ remove __init_subclass from the dir attributes"""
+        """ remove the init subclass"""
         attributes = super().__dir__()
         list_to_return = []
         for att in attributes:
             if att != "__init_subclass__":
                 list_to_return.append(att)
         return list_to_return
+
+class BaseGeometry(metaclass=BaseGeometry):
+
+    """ Metaclasses allow for code to manipulate classes.
+    Often this change happens when an object of the class is instantiated
+    """
+
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
+
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
-        """ validate the value if it is an intereger or not
-        and if it's negative or not """
         self.__name = name
         self.__value = value
         if isinstance(value, int):
