c559aa4956457a548edb7b7658881e9d40b74e7b
Roza Alene <rozaalene@gmail.com>
2023-08-03 11:38:25 -0700

    remove the __init_subclass

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,26 @@
 """ class: BaseGeometry """
 
+class BaseGeometry(type):
+    """ every thing in python is an object soo the class is also an object.
+    so if it is an object it have type.type is a metaclass, of which classes
+    are instances. Just as an ordinary object is an instance of a class, 
+    any new-style class in Python, and thus any class in Python 3, 
+    is an instance of the type metaclass """
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
 
-class BaseGeometry:
-    """ creating an empty class """
-    def __init__(self):
-        """ using pass"""
-        pass
+class BaseGeometry(metaclass=BaseGeometry):
+    
+    """ Metaclasses allow for code to manipulate classes.
+    Often this change happens when an object of the class is instantiated
+    """
+
     def __dir__(cls):
         """ remove the init subclass"""
         attributes = super().__dir__()
