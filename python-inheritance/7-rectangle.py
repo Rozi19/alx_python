52b1422d036c9cd7c07a1224528bb13a35cc9242
Roza Alene <rozaalene@gmail.com>
2023-08-03 11:57:51 -0700

    remove the __init_subclass

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -2,14 +2,40 @@
     child class: Rectangle"""
 
 
-class BaseGeometry:
-    """ creating an empty class """
-    def __init__(self):
-        """ using pass"""
-        pass
+class BaseGeometry(type):
+    """ every thing in python is an object soo the class is also an object.
+    so if it is an object it have type.type is a metaclass, of which classes
+    are instances. Just as an ordinary object is an instance of a class,
+    any new-style class in Python, and thus any class in Python 3,
+    is an instance of the type metaclass """
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
+
+class BaseGeometry(metaclass=BaseGeometry):
+
+    """ Metaclasses allow for code to manipulate classes.
+    Often this change happens when an object of the class is instantiated
+    """
+
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
+
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         self.__name = name
         self.__value = value
@@ -20,6 +46,8 @@ class BaseGeometry:
                 raise ValueError("{} must be greater than 0".format(self.__name))
         else:
             raise TypeError("{} must be an integer".format(self.__name))
+
+
 class Rectangle(BaseGeometry):
     """" the class inherit from BaseGeometry"""
     def __init__(self, width, height):
