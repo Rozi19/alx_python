3913e31036c2c5b4a8171db8b7134b6b05e2edf3
Roza Alene <rozaalene@gmail.com>
2023-08-07 14:07:01 +0300

    override _str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,7 @@ class Rectangle(Base):
             for y in range(self.__width):
                 print("#", end = "")
             print()
+
+    def __str__(self):
+        """ object representation in a string format """
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
