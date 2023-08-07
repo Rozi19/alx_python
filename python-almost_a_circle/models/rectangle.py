38306042bdd6d273b8696ca381ab8a1e62b2ab45
Roza Alene <rozaalene@gmail.com>
2023-08-07 11:13:49 +0300

    setter value

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,26 +12,27 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
     def get_width(self):
         return self.__width
     
-    def set_width(self):
-        return self.__width
+    def set_width(self, width):
+        self.__width = width
    
     def get_height(self):
         return self.__height
 
-    def set_height(self):
-        return self.__height
+    def set_height(self, height):
+        self.__height = height
   
     def get_x(self):
         return self.__x
     
-    def set_x(self):
-        return self.__x
+    def set_x(self, x):
+        self.__x = x
     
     def get_y(self):
         return self.__y
    
-    def set_y(self):
-        return self.__y
+    def set_y(self, y):
+        self.__y = y
