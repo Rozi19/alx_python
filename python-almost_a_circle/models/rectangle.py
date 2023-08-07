c7ac6609f4e628fde8bfb57fe02edc116a09a029
Roza Alene <rozaalene@gmail.com>
2023-08-07 11:48:01 +0300

    add documention for getter and setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,25 +14,33 @@ class Rectangle(Base):
         super().__init__(id)
 
     def get_width(self):
+        """ getter width """ 
         return self.__width
     
     def set_width(self, width):
+        """ setter x """
         self.__width = width
    
     def get_height(self):
+        """ getter height """
         return self.__height
 
     def set_height(self, height):
+        """ setter x """
         self.__height = height
   
     def get_x(self):
+        """ getter x """
         return self.__x
     
     def set_x(self, x):
+        """ setter x """
         self.__x = x
     
     def get_y(self):
+        """ getter y """
         return self.__y
    
     def set_y(self, y):
+        """ setter y """
         self.__y = y
