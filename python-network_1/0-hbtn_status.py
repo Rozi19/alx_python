f53e02e45602eeb3557c1547df9ef71a87e475b6
Roza Alene <rozaalene@gmail.com>
2023-08-09 20:20:35 +0300

    adding descripion

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
-""" import requests and send request """
+""" import requests and send get request 
+    then print the conent and type """
+
+
 import requests
 
 r = requests.get('https://alu-intranet.hbtn.io/status')
 r1 = r.text
 r2 = type(r.text)
-print("Body response:")
-                print("- type: {}".format(r2))
-                print("- content: {}".format(r1))
+print("Body response:\n""\t- type: {}\n""\t- content: {}".format(r3, r1))
