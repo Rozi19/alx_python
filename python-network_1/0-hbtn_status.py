d95e707ce024de3104c3a635efedc79d5fe763ae
Roza Alene <rozaalene@gmail.com>
2023-08-09 20:06:33 +0300

    adding new line and space

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,5 +5,5 @@ r = requests.get('https://alu-intranet.hbtn.io/status')
 r1 = r.text
 r2 = type(r.text)
 print("Body response:")
-print("- type: {}".format(r2))
-print("- content: {}".format(r1))
+                print("- type: {}".format(r2))
+                print("- content: {}".format(r1))
