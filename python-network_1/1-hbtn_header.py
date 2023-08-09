1e82439d252fe2919a7d6986651a123dbe6b1367
Roza Alene <rozaalene@gmail.com>
2023-08-09 21:54:14 +0300

    moudel document

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,5 @@
-""" import sys and requests
+""" 
+    import sys and requests
     sends a request to the URL and 
     displays the value of the variable X-Request-Id
 """
@@ -8,5 +9,4 @@ import requests
 
 r = requests.get(sys.stdin)
 r1 =  r.headers['X-Request-Id']
-
 print(r1)
