7a7adb58c5761889a9a8bbf299f5b911abfea2ad
Roza Alene <rozaalene@gmail.com>
2023-08-09 19:55:45 +0300

    adding content and type

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,3 +2,8 @@
 import requests
 
 r = requests.get('https://alu-intranet.hbtn.io/status')
+r1 = r.text
+r2 = type(r.text)
+print("Body response:")
+print("- type: {}".format(r2))
+print("- content: {}".format(r1))
