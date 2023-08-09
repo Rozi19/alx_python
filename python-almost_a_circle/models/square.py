fc23dad02ff5cd5e25070e744547ec0bd173fa9f
Roza Alene <rozaalene@gmail.com>
2023-08-09 13:26:03 +0300

    validate width and height

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,20 +9,29 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.__size = size
-        self.__x =  x
-        self.__y = y
+        self.size = size
+    
     @property
     def size(self):
         """ size getter """
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """size setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.height = value
+
         
     def __str__(self):
         """object(square) representation in a string format"""
-        return("[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.__size))
+        return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
