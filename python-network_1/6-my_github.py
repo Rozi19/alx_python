18210088f79962462206b52b8d8ff648e27daad2
Roza Alene <rozaalene@gmail.com>
2023-08-10 22:09:17 +0300

    define main

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -5,12 +5,12 @@ uses the GitHub API to display your id
 import requests
 import sys
 
-
-r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
-try:
-    print(r.json().get('id'))
-except ValueError:
-    print('Not a valid JSON')
+def main():
+    r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    try:
+        print(r.json().get('id'))
+    except ValueError:
+        print('Not a valid JSON')
 
 if __name__ == "__main__":
     main()
