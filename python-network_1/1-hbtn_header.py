1f4d456464d2382d89b84c62d6474a93a5b7c67a
Roza Alene <rozaalene@gmail.com>
2023-08-10 16:29:37 +0300

    not be executed when imported

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """ 
     import sys and requests
     sends a request to the URL and 
@@ -15,3 +13,6 @@ url = sys.argv[1]
 r = requests.get(url)
 r1 =  r.headers['X-Request-Id']
 print(r1)
+
+if __name__ == "__main__":
+     main()
