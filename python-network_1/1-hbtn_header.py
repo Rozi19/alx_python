768877b80048d1ea82427128c2169748dd31fefb
Roza Alene <rozaalene@gmail.com>
2023-08-10 17:25:06 +0300

    define the main

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,10 +8,10 @@
 
 import sys
 import requests
-
-url = sys.argv[1]
-r = requests.get(url)
-r1 =  r.headers['X-Request-Id']
-print(r1)
+def main():
+    url = sys.argv[1]
+    r = requests.get(url)
+    r1 =  r.headers['X-Request-Id']
+    print(r1)
 if __name__ == "__main__":
      main()
