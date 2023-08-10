d6a3c3f9e4303fbcd5c45325bfd78f9c4749b472
Roza Alene <rozaalene@gmail.com>
2023-08-10 17:11:09 +0300

    not be executed when imported

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,26 @@
+""" 
+    Takes in a letter and sends a POST request 
+    To http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+import sys
+import requests
+
+url = "http://0.0.0.0:5000/search_user"
+q = str.argv[1] if len(sys.argv) > 1 else ""
+
+r = requests.get(url, data = {'q': q})
+try:
+    r1 = response.json()
+
+    if r1:
+        print("[{}] {}".format(data.get('id'), data.get('name')))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
+
+if __name__ == "__main__":
+     main()
+
