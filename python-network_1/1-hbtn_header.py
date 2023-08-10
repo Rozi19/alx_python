56b3101761b5a1eeade1cd7cdba40b3c8841932d
Roza Alene <rozaalene@gmail.com>
2023-08-10 17:46:59 +0300

    check x-request id is in header or not

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -11,7 +11,10 @@ import requests
 def main():
     url = sys.argv[1]
     r = requests.get(url)
-    r1 =  r.headers.get['X-Request-Id']
-    print(r1)
+    if 'X-Request-Id' in r.headers:
+        r1 =  r.headers['X-Request-Id']
+        print(r1)
+    else:
+        print("None")
 if __name__ == "__main__":
      main()
