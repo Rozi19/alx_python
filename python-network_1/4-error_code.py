d39989eab9a5f6f6cb70bd2064b2b545892f18b2
Roza Alene <rozaalene@gmail.com>
2023-08-10 18:27:38 +0300

    not be executed when imported

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -2,11 +2,20 @@
     sends a request to the URL
     displays the body of the response. """
 
+
 import sys
 import requests
 
-url = sys.argv[1]
-r = requests.get(url)
-r1 = r.status_code
-if r1 >= 400:
-    print("Error code: {}".format(r1))
+def main():
+    url = sys.argv[1]
+    r = requests.get(url)
+
+    r1 = r.status_code
+
+    if r1 >= 400:
+        print("Error code: {}".format(r1))
+    elif r1 < 400:
+        print("Regular request")
+    
+if __name__ == "__main__":
+     main()
