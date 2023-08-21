d583163d8951c487f855d0de847e8d535e5456ce
Roza Alene <rozaalene@gmail.com>
2023-08-21 19:07:57 +0300

    remove the main

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,22 +6,19 @@ import MySQLdb
 import sys
 
 
-def main():
-    """
+"""
     connecting with the data base
     #execute statements to communicate with the database
     #execute our query
     #put the resule on a variable
     print our result
-    """
-    U = sys.argv[1]
-    pw = sys.argv[2]
-    d = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306, user=U, passwd=pw, db=d)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    result = cur.fetchall()
-    for state in result:
-        print(state)
-if __name__ == "__main__":
-    main()
+"""
+U = sys.argv[1]
+pw = sys.argv[2]
+d = sys.argv[3]
+db = MySQLdb.connect(host="localhost", port=3306, user=U, passwd=pw, db=d)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+result = cur.fetchall()
+for state in result:
+    print(state)
