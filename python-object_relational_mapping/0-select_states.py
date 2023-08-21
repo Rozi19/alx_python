daab3256ebb186dbd76d8913eb0cb68d33e3c9bb
Roza Alene <rozaalene@gmail.com>
2023-08-21 19:01:18 +0300

    some line and space correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,24 @@
-# list all elemnt from states table in python 
+# list all elemnt from states table in python
 #import mysqldb abd sys
+
 
 import MySQLdb
 import sys
 
+
 def main():
-#connecting with the data base
-    mysqlUname = sys.argv[1]
-    mysqlpw = sys.argv[2]
-    dbname = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306, user= mysqlUname, passwd= mysqlpw, db= dbname)
-#execute statements to communicate with the database
+    #connecting with the data base
+    U = sys.argv[1]
+    pw = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=U, passwd=pw, db=d)
+    #execute statements to communicate with the database
     cur = db.cursor()
-#execute our query
+    #execute our query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-#put the resule on a variable
+    #put the resule on a variable
     result = cur.fetchall()
-#print our result
+    #print our result
     for state in result:
         print(state)
 if __name__ == "__main__":
