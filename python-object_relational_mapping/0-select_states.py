13433b9fa3a321832f398ff82760b19afdefc630
Roza Alene <rozaalene@gmail.com>
2023-08-21 19:05:04 +0300

    write the comment together

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,26 @@
-# list all elemnt from states table in python
-#import mysqldb abd sys
-
+"""list all elemnt from states table in python
+import mysqldb abd sys
+"""
 
 import MySQLdb
 import sys
 
 
 def main():
-    #connecting with the data base
+    """
+    connecting with the data base
+    #execute statements to communicate with the database
+    #execute our query
+    #put the resule on a variable
+    print our result
+    """
     U = sys.argv[1]
     pw = sys.argv[2]
     d = sys.argv[3]
     db = MySQLdb.connect(host="localhost", port=3306, user=U, passwd=pw, db=d)
-    #execute statements to communicate with the database
     cur = db.cursor()
-    #execute our query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    #put the resule on a variable
     result = cur.fetchall()
-    #print our result
     for state in result:
         print(state)
 if __name__ == "__main__":
