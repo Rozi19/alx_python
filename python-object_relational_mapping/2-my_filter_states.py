b265546db015cc9ce834b342f763cec0f1ba00b1
Roza Alene <rozaalene@gmail.com>
2023-08-22 22:39:11 +0300

    space

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ def main():
     db = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=pw, db=d)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name= '{}' COLLATE utf8mb4_bin"
-    .format(stat))
+                .format(stat))
     result = cur.fetchall()
     for statess in result:
         print(statess)
