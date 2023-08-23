58dc2ee20e77803253a3ea9c02b5b87654197305
Roza Alene <rozaalene@gmail.com>
2023-08-23 16:42:00 +0300

    class document

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,27 +8,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import column, Integer, String
 
 
+"""declarative_base() callable returns a new base class from 
+which all mapped classes should inherit.
+connecting with the database and take values from the argument"""
+
 user =sys.argv[1]
 passwd = sys.argv[2]
 db1 = sys.argv[3]
-""" connecting with the database and take values from the argument"""
+
 db = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
     .format(user, passwd, db1), echo = TRUE)
-
-"""declarative_base() callable returns a new base class from 
-which all mapped classes should inherit."""
 Base = declarative_base()
 """ create class state """
 class State(Base):
     """ contains the table to be mapped to, and names 
-and datatypes of columns in it."""
+        and datatypes of columns in it."""
 
     __tablename__ = 'states'
     id = column(Integer, primary_key=True,
         autoincrement = True, unique = True, nullable = False)
     name = column(String(128), nullable = False)
-    
-""".metadata attribute of declarative base class
+
+"""
+metadata attribute of declarative base class
 MetaData.create_all() method is, 
 passing in our Engine as a source of database connectivity.
 """
