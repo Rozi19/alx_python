c7b74e7f16724d51a2d0418de6c50395d9aec04e
Roza Alene <rozaalene@gmail.com>
2023-08-23 18:33:13 +0300

    comment at one place

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 """
 class definition of a State and an instance Base = declarative_base()
-"""
+declarative_base() callable returns a new base class from 
+which all mapped classes should inherit.
+connecting with the database and take values from the argument"""
 
 import sys
 from sqlalchemy import create_engine
@@ -8,18 +10,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import column, Integer, String
 
 
-"""declarative_base() callable returns a new base class from 
-which all mapped classes should inherit.
-connecting with the database and take values from the argument"""
-
 user =sys.argv[1]
 passwd = sys.argv[2]
 db1 = sys.argv[3]
-
 db = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
     .format(user, passwd, db1), echo = TRUE)
 Base = declarative_base()
-""" create class state """
 class State(Base):
     """ 
         contains the table to be mapped to, and names 
