dbc45eac9961fc0293556618979ac654e4649387
Roza Alene <rozaalene@gmail.com>
2023-08-23 18:45:47 +0300

    add main

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,17 +9,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy import column, Integer, String
 
-
-user =sys.argv[1]
-passwd = sys.argv[2]
-db1 = sys.argv[3]
-db = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-    .format(user, passwd, db1), echo = TRUE)
-Base = declarative_base()
-class State(Base):
-    """ contains the table to be mapped to, and name"""
-    __tablename__ = 'states'
-    id = column(Integer, primary_key=True,
-        autoincrement = True, unique = True, nullable = False)
-    name = column(String(128), nullable = False)
-Base.metadata.create_all(db)
+if __name__ == "__main__":
+    user =sys.argv[1]
+    passwd = sys.argv[2]
+    db1 = sys.argv[3]
+    db = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+        .format(user, passwd, db1), echo = True)
+    Base = declarative_base()
+    class State(Base):
+        """ contains the table to be mapped to, and name"""
+        __tablename__ = 'states'
+        id = column(Integer, primary_key=True,
+            autoincrement = True, unique = True, nullable = False)
+        name = column(String(128), nullable = False)
+    Base.metadata.create_all(db)
