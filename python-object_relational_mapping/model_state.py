94580dd5bb34e8dd0dc17d15ff68bd97e224c7b6
Roza Alene <rozaalene@gmail.com>
2023-08-23 19:07:02 +0300

    remove metadata

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,18 +2,18 @@
 class definition of a State and an instance Base = declarative_base()
 declarative_base() callable returns a new base class from 
 which all mapped classes should inherit.
-connecting with the database and take values from the argument"""
-
+"""
 
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy import column, Integer, String
 
 
 Base = declarative_base()
+
+
 class State(Base):
     """ contains the table to be mapped to, and name"""
     __tablename__ = 'states'
     id = column(Integer, primary_key=True,
-            autoincrement = True, unique = True, nullable = False)
+        autoincrement = True, unique = True, nullable = False)
     name = column(String(128), nullable = False)
-Base.metadata.create_all(db)
