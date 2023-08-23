303294d15031b32c2f97bdb8ec9055c4050e6b1f
Roza Alene <rozaalene@gmail.com>
2023-08-23 19:35:07 +0300

    upper case

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ which all mapped classes should inherit.
 """
 
 from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy import column, Integer, String
+from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
