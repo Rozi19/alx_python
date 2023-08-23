e216bb742511bb02b8a01e1488253b9fe4e5a27b
Roza Alene <rozaalene@gmail.com>
2023-08-23 19:03:03 +0300

    with out takeing argument

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,22 +4,16 @@ declarative_base() callable returns a new base class from
 which all mapped classes should inherit.
 connecting with the database and take values from the argument"""
 
-import sys
-from sqlalchemy import create_engine
+
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy import column, Integer, String
 
-if __name__ == "__main__":
-    user =sys.argv[1]
-    passwd = sys.argv[2]
-    db1 = sys.argv[3]
-    db = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-        .format(user, passwd, db1), echo = True)
-    Base = declarative_base()
-    class State(Base):
-        """ contains the table to be mapped to, and name"""
-        __tablename__ = 'states'
-        id = column(Integer, primary_key=True,
+
+Base = declarative_base()
+class State(Base):
+    """ contains the table to be mapped to, and name"""
+    __tablename__ = 'states'
+    id = column(Integer, primary_key=True,
             autoincrement = True, unique = True, nullable = False)
-        name = column(String(128), nullable = False)
-    Base.metadata.create_all(db)
+    name = column(String(128), nullable = False)
+Base.metadata.create_all(db)
