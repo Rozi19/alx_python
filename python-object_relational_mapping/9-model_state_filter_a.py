140bc12b9f8ad82b1e46b796d10f4797dd200b3d
Roza Alene <rozaalene@gmail.com>
2023-08-25 12:45:56 +0300

    order by

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     session = Session()
 
     # Query all State objects and filter the state name contins a
-    state = session.query(State).filter(States.name.like('%a%')).all()
+    state = session.query(State).filter(States.name.like('%a%')).order_by(State.id).all()
 
     # Display the results
     for state1 in state:
