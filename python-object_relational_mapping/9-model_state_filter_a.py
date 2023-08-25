b024aea69cf18b30366ac806a52fb16073d089e6
Roza Alene <rozaalene@gmail.com>
2023-08-25 12:31:33 +0300

    remove the persont sign

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,26 +5,24 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
-# Get MySQL username, password, and database
+    # Get MySQL username, password, and database
     username = sys.argv[1]
     passwd = sys.argv[2]
     database = sys.argv[3]
 
-# Create the database engine
+    # Create the database engine
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         username, passwd, database))
  
- # Create a session factory
+    # Create a session factory
     Session = sessionmaker(bind=engine)
 
-# Create a session
+    # Create a session
     session = Session()
 
-# Query all State objects and filter the state name contins a
-    state = session.query(State).filter(States.name.contains('%a')).all()
+    # Query all State objects and filter the state name contins a
+    state = session.query(State).filter(States.name.contains('a')).all()
 
- # Display the results
+    # Display the results
     for state1 in state:
         print("{}: {}".format(state.id, state.name))
-
-
