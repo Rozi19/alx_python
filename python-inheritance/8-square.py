8cccabe0d77b488a60788c26d9d64ba6cbfbf4ee
Roza Alene <rozaalene@gmail.com>
2023-09-02 12:49:53 +0300

    rename

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """use the rectangle methods"""
         super().__init__(size, size)
-        self.ize = size
+        self.size = size
         self.integer_validator("size", self.size)
         self.__str__()
         self.area()
