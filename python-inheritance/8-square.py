ab8565f0c76911a00f8b4836eb8b1dc8c0bcad55
Roza Alene <rozaalene@gmail.com>
2023-09-02 12:34:38 +0300

    super to replace the parent class value

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -6,8 +6,8 @@ Rectangle = __import__('7-rectangle').Rectangle
 class Square(Rectangle):
     """ the class inherite from rectangle """
     def __init__(self, size):
-        """useing the rectangle methods"""
-        Rectangle.__init__(self, size, size)
+        """use the rectangle methods"""
+        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", self.__size)
         self.__str__()
