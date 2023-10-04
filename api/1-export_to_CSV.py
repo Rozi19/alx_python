0f5cf4a5f73d8f2824aa43f997e2ee89ec8b83f0
Roza Alene <rozaalene@gmail.com>
2023-10-04 04:17:49 +0300

    editing some speling error

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,24 +7,22 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    emplooye_id = sys.argv[1]
-    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(emplooye_id))
-    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(emplooye_id))
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
     data = api_request.text
     pjson = json.loads(data)
     data1 = api_request1.text
     pjson1 = json.loads(data1)
 
-    title_count = 0
-    title_complete = 0
     name_info = pjson['name']
 
     #export data to csv data
-    filename = "{}.csv".format(emplooye_id)
+    filename = "{}.csv".format(employee_id)
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
         for item in pjson1:
-            user_id = emplooye_id
+            user_id = employee_id
             username = name_info
             task_completed_status = item['completed']
             task_title = item['title']
