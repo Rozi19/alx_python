b0a0708ea8df20216d759b8e5a2d13e5495f6adc
Roza Alene <rozaalene@gmail.com>
2023-10-05 11:25:35 +0300

    username not name

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,16 +15,13 @@ if __name__ == "__main__":
     data1 = api_request1.text
     pjson1 = json.loads(data1)
 
-    name_info = pjson['name']
-
     #export data to csv data
     filename = "{}.csv".format(employee_id)
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
         for item in pjson1:
             user_id = employee_id
-            username = name_info
+            username = pjson['username']
             task_completed_status = item['completed']
             task_title = item['title']
             writer.writerow([user_id, username, task_completed_status, task_title])
-    print("data export to {}".format(filename))
